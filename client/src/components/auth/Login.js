bb4ecd345d23fe1c4b60d0a9c8b181f00dbd4f9b
Julio Alonso <jalonso4078@gmail.com>
2020-03-27 09:04:24 -0700

    theme coloring

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,20 +1,23 @@
-import React, { useState } from 'react'
-import { useAuth } from '../../lib/react-auth'
+import React, { useState } from "react"
+import { useAuth } from "../../lib/react-auth"
+//import "../../styles/login.css"
 
-import { Button, Form, Input, Label } from 'semantic-ui-react'
-import 'semantic-ui-css/semantic.min.css'
+import { Button, Form, Input, Label } from "semantic-ui-react"
+import "semantic-ui-css/semantic.min.css"
 
 export default props => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
   const { signin } = useAuth()
 
   function handleSubmit(e) {
     e.preventDefault()
 
-    signin(username, password).then(resp => {
-      props.history.push('/inventory')
-    }).catch(err => console.log('ERROR', err))
+    signin(username, password)
+      .then(resp => {
+        props.history.push("/inventory")
+      })
+      .catch(err => console.log("ERROR", err))
   }
 
   return (
@@ -22,13 +25,22 @@ export default props => {
       <Form onSubmit={handleSubmit}>
         <Form.Field>
           <Label color="grey">Username</Label>
-          <Input value={username} onChange={e => setUsername(e.target.value)} placeholder="username" />
+          <Input
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+            placeholder="username"
+          />
         </Form.Field>
         <Form.Field>
           <Label color="grey">Password</Label>
-          <Input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="password" />
+          <Input
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            placeholder="password"
+          />
         </Form.Field>
-        <Button type='submit'>Submit</Button>
+        <Button type="submit">Submit</Button>
       </Form>
     </div>
   )
--- client/src/components/home/Contact.js
+++ client/src/components/home/Contact.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react"
 
-import Nav from './index'
+import Nav from "./index"
 
 export default props => {
   return (
