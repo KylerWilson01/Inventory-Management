a4f520a4f5ffa5e4c4fb9731a2711991323378fe
Julio Alonso <jalonso4078@gmail.com>
2020-03-27 14:32:29 -0700

    theme cleanup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import { AuthProvider, AuthRoute } from "../lib/react-auth"
-
+import "../styles/base.css"
 import Auth from "./auth/Auth"
 import Inventory from "./inventory/Inventory"
 import CatForm from "./inventory/Cat"
@@ -12,16 +12,21 @@ import Contact from "./home/Contact"
 
 export default props => {
   return (
-    <AuthProvider>
-      <Router>
-        <Route exact path="/" component={Home} />
-        <Route path="/about-us" component={About} />
-        <Route path="/contact-us" component={Contact} />
-        <Route path="/login" component={Auth} />
-        <AuthRoute exact path="/inventory" component={Inventory} />
-        <AuthRoute path="/inventory/:username/new-tab" component={CatForm} />
-        <AuthRoute path="/inventory/:username/new-item" component={ItemForm} />
-      </Router>
-    </AuthProvider>
+    <div className="appWrapper">
+      <AuthProvider>
+        <Router>
+          <Route exact path="/" component={Home} />
+          <Route path="/about-us" component={About} />
+          <Route path="/contact-us" component={Contact} />
+          <Route path="/login" component={Auth} />
+          <AuthRoute exact path="/inventory" component={Inventory} />
+          <AuthRoute path="/inventory/:username/new-tab" component={CatForm} />
+          <AuthRoute
+            path="/inventory/:username/new-item"
+            component={ItemForm}
+          />
+        </Router>
+      </AuthProvider>
+    </div>
   )
 }
