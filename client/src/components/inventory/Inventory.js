709ec3dfee1ff83cc0f5ee8519f4ad9cc6c65dc7
Kyler Wilson <kyleraw2001@gmail.com>
2020-04-03 15:53:26 -0700

    switches between themes

--- client/src/components/inventory/Inventory.js
+++ client/src/components/inventory/Inventory.js
@@ -1,8 +1,9 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { useAuth, useCats } from "../../hooks"
-import { Tab, Button, Icon, Menu, Input } from "semantic-ui-react"
+import { Tab, Button, Icon, Menu, Input, Modal } from "semantic-ui-react"
 
-import "../../styles/inventory.scss"
+import "../../styles/darkmode.scss"
+import "../../styles/lightmode.scss"
 
 import NewCat from "./NewCat"
 import Cat from './Cat'
@@ -10,6 +11,16 @@ import Cat from './Cat'
 export default props => {
   const { profile, signout } = useAuth()
   const { categories, results, delCat, getCats, search } = useCats()
+  const [darkMode, setDarkMode] = useState(getInitialMode())
+
+  useEffect(() => {
+    localStorage.setItem('mode', JSON.stringify(darkMode))
+  }, [darkMode])
+
+  function getInitialMode() {
+    const colorMode = JSON.parse(localStorage.getItem('mode'))
+    return colorMode ? colorMode : true
+  }
 
   function handleCatDel(e) {
     e.preventDefault()
@@ -25,7 +36,7 @@ export default props => {
       search(profile.username, null)
     }
   }
-  
+
   const panes = results[0] ? results.map((cat, i) => ({
     menuItem: (
       <Menu.Item key={'cat-' + i}>
@@ -51,10 +62,23 @@ export default props => {
   }))
 
   return (
-    <div className="inventory">
+    <div className={darkMode ? "dark-mode" : "light-mode"} >
       <header>
-        <h1>{profile.username}</h1>
-        <Button onClick={e => signout()}>Sign out</Button>
+        <Modal
+          trigger={<Button>{profile.username}</Button>}
+          header='User Settings'
+          closeIcon
+          content={
+            <>
+              <Button
+                onClick={e => setDarkMode(prevMode => !prevMode)}>
+                {darkMode ? 'Change to light mode' : 'Change to dark mode'}
+              </Button>
+              <Button onClick={e => signout()}>Sign out</Button>
+            </>
+          }
+          actions={['Snooze', { key: 'done', content: 'Done', positive: true }]}
+        />
         <NewCat />
       </header>
       <br />
